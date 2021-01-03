8098b9b3094a1fc4d9489108bb9a10397b0e99f0
Jason <jason.lo.kc@gmail.com>
2021-01-02 23:32:44 -0800

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -9,13 +9,14 @@ const About = () => {
   return (
     <div className="about">
       <div className="about__content">
-        <h2 className="about__content--mission">ABOUT ME</h2>
+        <h2 className="about__content--mission">About</h2>
         <Typist avgTypingDelay className="about__content--missiontext">
           As a developer, it is my goal to help dreamers, entrepreneurs, and
           businesses to bring their ideas into reality on web/mobile platforms
           for the betterment of our society.
         </Typist>
       </div>
+      <div className="about__resume">Resume</div>
       <div className="about__valuetitle">MY CORE VALUES</div>
       <div className="about__values">
         <div className="about__values--1">
@@ -26,6 +27,10 @@ const About = () => {
               alt="codeicon"
             />
             <div className="about__values--value">Clean Code</div>
+            <div className="about__values--details">
+              I ensure my code are elegantly written, easy to read, and
+              efficiently written
+            </div>
           </div>
           <div className="about__values--section">
             <img
@@ -34,6 +39,10 @@ const About = () => {
               alt="responsiveicon"
             />
             <div className="about__values--value">Responsive</div>
+            <div className="about__values--details">
+              The website will change fit for comprehensibility depending on the
+              user's device
+            </div>
           </div>
         </div>
         <div className="about__values--2">
@@ -44,6 +53,9 @@ const About = () => {
               alt="timeicon"
             />
             <div className="about__values--value">Time Efficient</div>
+            <div className="about__values--details">
+              Fast loading speed all across the website
+            </div>
           </div>
           <div className="about__values--section">
             <img
@@ -52,6 +64,10 @@ const About = () => {
               alt="improveicon"
             />
             <div className="about__values--value">Improvment</div>
+            <div className="about__values--details">
+              Technologies are ever evolving, as a developer I strive to
+              constantly improve to stay on top of the change{" "}
+            </div>
           </div>
         </div>
       </div>
